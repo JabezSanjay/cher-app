89e277535e780a63c037cc12391206626730af03
RishikeshTbr <tbr30929@gmail.com>
2021-08-30 15:29:00 +0530

    completed button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BrandButton from "./Components/BrandButton";
 import BrandImageDiv from "./Components/BrandImageDiv";
 import BrandInput from "./Components/BrandInput";
@@ -6,9 +6,13 @@ import BrandInput from "./Components/BrandInput";
 const App = () => {
   return (
     <div>
-      <div className="m-10">
+      <div className="m-10 flex">
         <BrandButton buttonName="yes" buttonValue={true} />
         <BrandButton buttonName="no" buttonValue={false} />
+      </div>
+      <div className="m-10 flex">
+        <BrandButton buttonName="Married" buttonValue={true} />
+        <BrandButton buttonName="Single" buttonValue={false} />
       </div>
       <div className="m-10">
         <BrandImageDiv />
