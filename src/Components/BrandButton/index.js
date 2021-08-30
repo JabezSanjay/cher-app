f1f8a0a5827025d180edaecbab5f2f2becec3a34
RishikeshTbr <tbr30929@gmail.com>
2021-08-30 15:29:41 +0530

    minor chanes

--- src/Components/BrandButton/index.js
+++ src/Components/BrandButton/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
-const BrandButton = ({ buttonName, buttonValue }) => {
+const BrandButton = ({ buttonName, buttonValue, setActive, active }) => {
   const [selectedValue, setSelectedValue] = useState(buttonValue);
 
   return (
     <div>
       <button
         onClick={() => {
-          console.log(selectedValue);
+          // console.log(selectedValue);
           setSelectedValue(buttonValue);
         }}
-        className="text-3xl"
+        className={`p-3 w-20 m-4 text-white rounded-lg bg-indigo-800 hover:bg-indigo-600 `}
       >
         {buttonName}
       </button>
